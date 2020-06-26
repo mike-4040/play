73b9e029f9f89170bf41be19f78b14205537bb4b
Mike Kravtsov <mike@clickup.com>
2020-06-26 07:15:24 -0700

    1 edit in master

--- app.js
+++ app.js
@@ -1,14 +1,3 @@
-class MyClass  {
-    #a = 10;
-    b = this.#a;
-}
-
-const myObj = new MyClass();
-
-
-
-console.log(myObj.#a);
-
-
-//initial file
-// commit 1
+/**
+ * 1. initial edit in master
+ * */
