080191c0960011965496608b41b059706757271d
Mike Kravtsov <mike4040@me.com>
2020-07-12 08:24:34 -0700

    c in f

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 let i = 0;
-setInterval(() => console.log(`hello: ${i++}.`), 1000);
+setInterval(() => console.log(`Hello: ${i++}.`), 1000);
 
 let j = 0;
 setInterval(() => console.log(`hello: ${j++}.`), 2000);
