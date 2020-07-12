5f87707c9df8bf00a572508a358666a7cb6c32d3
Mike Kravtsov <mike4040@me.com>
2020-07-12 08:33:47 -0700

    feature

--- app.js
+++ app.js
@@ -2,4 +2,4 @@ let i = 0;
 setInterval(() => console.log(`hello from master: ${i++}.`), 1000);
 
 let j = 0;
-setInterval(() => console.log(`hello: ${j++}.`), 2000);
+setInterval(() => console.log(`hello from feature: ${j++}.`), 2000);
