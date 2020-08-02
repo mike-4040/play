ad709a189c1b5b64266421ad8c7f77fe091d61bc
Mike Kravtsov <mike4040@me.com>
2020-08-02 06:40:18 -0700

    change in master

--- app.js
+++ app.js
@@ -3,3 +3,6 @@ setInterval(() => console.log(`hello from master: ${i++}. Change in master`), 10
 
 let j = 0;
 setInterval(() => console.log(`hello from feature: ${j++}.`), 2000);
+
+// change in master
+
