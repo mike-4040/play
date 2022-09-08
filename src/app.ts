c9b86a87a28a73b2f9fc5a5af56e8d8b20680499
Mike Kravtsov <mike.k@quo.com>
2022-09-07 18:42:00 -0700

    testing

---
+++ src/app.ts
@@ -0,0 +1,12 @@
+const someObject = {
+    prop1: "prop1",
+    prop2: "prop2",
+  };
+
+  console.log({ someObject });
+
+  Object.freeze(someObject);
+
+  someObject.prop1 = "new value";
+
+  console.log({ someObject });
