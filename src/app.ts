15f898cdaf0da1842798862aac1bc5859ec57e91
Mike Kravtsov <mike.k@quo.com>
2022-11-10 11:08:13 -0800

    server setup

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,5 @@
+import express from 'express';
 
-const myObj = {} as Record<string, string>;
-myObj["1"] = "one";
-Object.assign(myObj, { 1: "two" });
-myObj[1] = "three";
-
-console.log(myObj);
-
-const myMap = new Map();
-myMap.set("1", "one");
-myMap.set(1, "two");
-myMap.set(1, "three");
-
-console.log(myMap);
-
+export default express()
+  .set('port', process.env.PORT || 3000)
+  .get('/', (_req, res) => res.send('Hello World!'));
