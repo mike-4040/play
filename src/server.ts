d2100243ce6299418470a96c2efef7d34e5825fe
Mike Kravtsov <mike.k@quo.com>
2022-11-12 14:07:53 -0800

    test

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,16 @@
-import dotenv from 'dotenv';
-dotenv.config();
+// import dotenv from 'dotenv';
+// dotenv.config();
 
-import app from './app.js';
+// import app from './app.js';
 
-app.listen(app.get('port'), () =>
-  console.log(`Server running on port ${app.get('port')}`)
-);
+// app.listen(app.get('port'), () =>
+//   console.log(`Server running on port ${app.get('port')}`)
+// );
+
+console.time()
+const iters = 10_000_000_000;
+for ( let i =0; i < iters; i++) {
+  Math.random( )
+
+}
+console.timeEnd()
