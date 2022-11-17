04a7c7a8fa52c9e64a94e95c1058716196b7c9a3
Mike Kravtsov <mike4040@me.com>
2022-11-16 16:11:19 -0800

    cleanup

---
+++ src/index.ts
@@ -0,0 +1,6 @@
+import { sum } from './dep.js';
+
+
+const r = sum(1, 2);
+
+console.log({ r });
--- src/server.ts
+++
@@ -1,9 +0,0 @@
-import dotenv from 'dotenv';
-dotenv.config();
-
-import app from './app.js';
-
-app.listen(app.get('port'), () =>
-  console.log(`Server running on port ${app.get('port')}`)
-);
-
